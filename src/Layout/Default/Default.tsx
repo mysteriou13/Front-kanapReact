295b6fdf11638448d479bf019a4b204f79039efe
Massa anthony <elio007@hotmail.fr>
2025-05-22 05:28:40 +0200

    ajout react router

---
+++ src/Layout/Default/Default.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router"
+
+export default function Default() {
+  return (
+    <div>
+    Default
+    
+    <Outlet/>
+    </div>
+
+)
+
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import Acceuil from "./Component/Acceuil/Acceuil";
 import Test from "./Component/Test/Test";
-
-
-export  const router = createBrowserRouter([
+import Default from "./Layout/Default/Default";
+export const router = createBrowserRouter([
   {
     path: "/",
-    Component: Acceuil,
-  },
-  {
-    path: "/test",
-    Component: Test,
+    element: <Default/>, // Le layout principal
+    children: [
+      {
+        index: true,
+        element: <Acceuil />,
+      },
+      {
+        path: "test",
+        element: <Test />,
+      },
+    ],
   },
 ]);
 
